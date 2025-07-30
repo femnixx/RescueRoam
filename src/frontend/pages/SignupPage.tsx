2ff6c2c082cafabc800dc33db139e361b7d64c35
femnix <femnitality@gmail.com>
2025-07-30 18:39:03 +0700

    Signup frontend slicing ready for backend testing.

--- src/frontend/pages/SignupPage.tsx
+++ src/frontend/pages/SignupPage.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return (
+    <>
+      <div className="">
+        <h1 className="text-center mt-5">Create An Account</h1>
+        <div className="ps-5 mt-5">
+          <p>Username</p>
+          <input type="username" className="border-1 rounded-lg" />
+        </div>
+        <div className="ps-5 mt-5">
+          <p>Email</p>
+          <input type="email" className="border-1 rounded-lg" />
+        </div>
+        <div className="ps-5 mt-5">
+          <p>Password</p>
+          <input type="password" className="border-1 rounded-lg" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SignupPage;
