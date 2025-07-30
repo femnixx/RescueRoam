1ed6de7531aee2c9ab479b25031603960e294b05
femnix <femnitality@gmail.com>
2025-07-30 18:36:01 +0700

    FInished login page for basic backend logic testing

--- src/frontend/pages/LoginPage.tsx
+++ src/frontend/pages/LoginPage.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <>
+      <div className="">
+        <h1 className="text-center mt-5">Welcome back</h1>
+        <div className=" flex flex-col gap-y-5 mt-5">
+          <div className="ps-5">
+            <p>Email</p>
+            <input type="email" className="border-1 rounded-lg" />
+          </div>
+          <div className="ps-5">
+            <p>Password</p>
+            <input type="password" className="border-1 rounded-lg" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LoginPage;
