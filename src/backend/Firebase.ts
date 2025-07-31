dae62e6a1ef297d089fab54e5bfbb4c5652f472a
femnixx <@femnitality@gmail.com>
2025-07-31 20:02:58 +0700

    Made backend folder and Firebase.ts

---
+++ src/backend/Firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDZNbGFMJMtVMt2sRaLR_C5_wqRDHaIiFs",
+  authDomain: "rescueroam-dbc1a.firebaseapp.com",
+  projectId: "rescueroam-dbc1a",
+  storageBucket: "rescueroam-dbc1a.firebasestorage.app",
+  messagingSenderId: "717889592198",
+  appId: "1:717889592198:web:a842fae1f0a6574375ea07",
+  measurementId: "G-2M5M0WD1SD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
