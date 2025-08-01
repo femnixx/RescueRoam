5729dafd7811b106d2958ac95dfa94a0b7b9859c
femnixx <femnitality@gmail.com>
2025-08-01 17:57:22 +0700

    Added firebase npm and added firebase config for authentication and initialization

---
+++ src/backend/Firebase.ts
@@ -0,0 +1,22 @@
+import { FirebaseError, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+var firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  // The value of `databaseURL` depends on the location of the database
+  databaseURL: "https://DATABASE_NAME.firebaseio.com",
+  projectId: "PROJECT_ID",
+  // The value of `storageBucket` depends on when you provisioned your default bucket (learn more)
+  storageBucket: "PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+  // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+  measurementId: "G-MEASUREMENT_ID",
+};
+
+// initialize firebase
+const app = initializeApp(firebaseConfig);
+
+// initialize firebase auth 
+const auth = getAuth(app);
