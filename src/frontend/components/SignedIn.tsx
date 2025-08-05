692b0f87f38402b4b2c819cfbfe3718afa7060d1
femnixx <femnitality@gmail.com>
2025-08-05 14:34:57 +0700

    Added SignedIn.tsx instead of putting it in landing page.

---
+++ src/frontend/components/SignedIn.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import { db } from "../../backend/Firebase.ts"
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect } from "react";
+import {auth} from "../../backend/Firebase";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { FirebaseError } from "firebase/app";
+
+const SignedIn = () => {
+const user = auth.currentUser;
+const userID = user?.uid;
+
+const [username, setUsername] = useState("Guest");
+useEffect(() => {
+    const fetchUsername = async () => {
+      if (userID) {
+        try {
+          const docRef = doc(db, "users", userID);
+          const docSnap = await getDoc(docRef);
+
+          if (docSnap.exists()) {
+            const userData = docSnap.data();
+            setUsername(userData.displayName);
+            console.log("Document data: ", userData);
+          } else {
+            console.log("Such document does not exist.")
+          }
+        } catch (error) {
+          console.error("Error fetching user document:", error);
+        }
+      }
+    };
+    fetchUsername();
+  }, [userID]);
+
+    // handle signout
+    const handleLogOut = () => {
+      try {
+        signOut(auth);
+        if (!user) {
+          window.alert('Successfully logged out.')
+          console.log("User session ended.")
+        } 
+      } catch (error) {
+        if (FirebaseError) {
+          console.log("Firebase error occured: ", FirebaseError)
+        } else {
+          console.log("Unexpted error occured: ", error)
+        }
+      }
+    }
+  
+
+    
+  return (
+      <>
+      <div className="flex gap-x-5 mt-5 justify-between mx-5">
+        <p>Currently signed in as: {username}</p>
+        <form onClick={handleLogOut}><button className="hover:cursor-pointer border-1 rounded-lg ps-5">Sign out</button></form>
+        </div>
+        </>
+    )
+}
+
+export default SignedIn
--- src/frontend/pages/LandingPage.tsx
+++ src/frontend/pages/LandingPage.tsx
@@ -1,12 +1,7 @@
-import { onAuthStateChanged, signOut } from "firebase/auth";
 import React, { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import {auth} from "../../backend/Firebase";
-import firebase from "firebase/compat/app";
-import { FirebaseError } from "firebase/app";
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "../../backend/Firebase.ts" 
-import { useEffect } from "react";
+import SignedIn from "../components/SignedIn";
 
 const LandingPage = () => {
   const user = auth.currentUser;
@@ -15,53 +10,10 @@ const LandingPage = () => {
   const displayName = user?.displayName;
   const [username, setUsername] = useState('Guest');
 
-  useEffect(() => {
-    const fetchUsername = async () => {
-      if (userID) {
-        try {
-          const docRef = doc(db, "users", userID);
-          const docSnap = await getDoc(docRef);
-
-          if (docSnap.exists()) {
-            const userData = docSnap.data();
-            setUsername(userData.displayName);
-            console.log("Document data: ", userData);
-          } else {
-            console.log("Such document does not exist.")
-          }
-        } catch (error) {
-          console.error("Error fetching user document:", error);
-        }
-      }
-    };
-    fetchUsername();
-  }, [userID]);
-
-
-  // handle signout
-  const handleLogOut = () => {
-    try {
-      signOut(auth);
-      if (!user) {
-        window.alert('Successfully logged out.')
-        console.log("User session ended.")
-      } 
-    } catch (error) {
-      if (FirebaseError) {
-        console.log("Firebase error occured: ", FirebaseError)
-      } else {
-        console.log("Unexpted error occured: ", error)
-      }
-    }
-  }
-
   if (user !== null) {
     return (
       <>
-      <div className="flex gap-x-5 mt-5 justify-between mx-5">
-        <p>Currently signed in as: {username}</p>
-        <form onClick={handleLogOut}><button className="hover:cursor-pointer border-1 rounded-lg ps-5">Sign out</button></form>
-        </div>
+        <SignedIn></SignedIn>
         </>
     )
   } else {
